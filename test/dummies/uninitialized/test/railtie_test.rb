f0a373dded94fb772017a9886accea6fd61d1927
Nick Sutterer <apotonick@gmail.com>
2023-07-27 16:15:04 +0200

    yay, we got tests for uninitialized rails app, and for a first-time install and go.

--- test/dummies/uninitialized/test/railtie_test.rb
+++ test/dummies/uninitialized/test/railtie_test.rb
@@ -15,8 +15,6 @@ class RailtieTest < Minitest::Spec
     Dir.chdir("test/dummies/uninitialized") do
       cli = File.popen({"BUNDLE_GEMFILE" => "../Gemfile"}, "bin/rails c", "r+")
       cli.write "WelcomeController.run_create\n"
-      # require "io/wait"
-      # cli.wait(nil, :read)
       lines = [
         cli.gets,
         cli.gets,
@@ -38,6 +36,32 @@ class RailtieTest < Minitest::Spec
 
       refute File.exist?("tmp/trb-pro/session")
       # assert_equal File.read("tmp/trb-pro/session"), %({"api_key":"ABC-999","trailblazer_pro_host":"https://pro.trailblazer.to"})
+    end
+  end
+
+  it "after configuration, you can trace on the web" do
+    Dir.chdir("test/dummies/uninitialized") do
+      cli = File.popen({"BUNDLE_GEMFILE" => "../Gemfile"}, "bin/rails g trailblazer:pro:install", "r+")
+      cli.write "tpka_f5c698e2_d1ac_48fa_b59f_70e9ab100604\n"
+      cli.close
+
+      json = File.read("tmp/trb-pro/session")
+      json = json.sub("https://pro.trailblazer.to", "http://localhost:3000")
+      File.write("tmp/trb-pro/session", json) # FIXME: what do you mean? This is super clean :D
+
+      cli = File.popen({"BUNDLE_GEMFILE" => "../Gemfile"}, "bin/rails c", "r+")
+      cli.write "WelcomeController.run_create\n"
+
+      line = nil
+      loop do
+        line = cli.gets
+        break if line =~ /\[TRB PRO]/
+      end
+      cli.close
+
+      puts "@@@@@ #{line.inspect}"
+
+      assert_equal line[0..-22], "[TRB PRO] view trace at https://ide.trailblazer.to/"
     end
   end
 
