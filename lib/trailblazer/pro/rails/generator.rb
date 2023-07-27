94660b49d48018e1cae1b69aa94766c571dec32c
Nick Sutterer <apotonick@gmail.com>
2023-07-27 10:28:58 +0200

    don't create invalid session file when CTRL-C is hit. better tests.

--- lib/trailblazer/pro/rails/generator.rb
+++ lib/trailblazer/pro/rails/generator.rb
@@ -2,13 +2,22 @@ module Trailblazer
 
   module Pro
     class Install < ::Rails::Generators::Base
-      source_root File.expand_path("templates", __dir__)
+      # source_root File.expand_path("templates", __dir__)
+
+      def prompt_for_api_key
+        input = ask %(Your Trailblazer PRO API key:)
+        # TODO: blank
+        return if input.blank?
+
+        @api_key = input.chomp
+      end
 
       def create_session_file
-        create_file Rails::SESSION_PATH do
-          input = ask %(Your Trailblazer PRO API key:)
+        return if @api_key.nil? # FIXME: use Trailblazer instead of thor's basic control flow.
 
-          uninitialized_session = Trailblazer::Pro::Session::Uninitialized.new(api_key: input, trailblazer_pro_host: "https://pro.trailblazer.to")
+        create_file Rails::SESSION_PATH do
+          uninitialized_session = Trailblazer::Pro::Session::Uninitialized.new(api_key: @api_key, trailblazer_pro_host: "https://pro.trailblazer.to")
+
           Trailblazer::Pro::Session.serialize(uninitialized_session)
         end
       end
