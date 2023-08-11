e3a633391d3306953b7647f3af68fe0c6ed4c1d6
Nick Sutterer <apotonick@gmail.com>
2023-08-11 07:38:08 +0200

    releasing 0.0.3.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.3]
+
+- Fix gemspec.
+
 ## [0.0.2]
 
 - Initial release
--- lib/trailblazer/pro/rails/version.rb
+++ lib/trailblazer/pro/rails/version.rb
@@ -3,7 +3,7 @@
 module Trailblazer
   module Pro
     module Rails
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
     end
   end
 end
